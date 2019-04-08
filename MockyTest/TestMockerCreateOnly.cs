b33969d5fe4e0cfedb56cf2cb366af9e80e9258b
PiniponSelvagem <piniponselvagem@gmail.com>
2019-04-08 13:37:24 +0100

    Fixed TestMockerCreateOnly using wrong HttpRequest.

--- MockyTest/TestMockerCreateOnly.cs
+++ MockyTest/TestMockerCreateOnly.cs
@@ -1,77 +1,64 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Net;
-using System.Collections.Generic;
-using Mocky;
 using Request;
 
-namespace Mocky.Test
-{
-    [TestClass]
-    public class TestMockerCreateOnly
-    {
-        readonly ICalculator calc;
-        readonly IHttpRequest req;
+namespace Mocky.Test {
 
-        public TestMockerCreateOnly()
-        {
+    [TestClass]
+    public class TestMockerCreateOnly {
+        readonly ICalculator calc;
+        readonly HttpRequest req;
+
+        public TestMockerCreateOnly() {
             Mocker mockCalc = new Mocker(typeof(ICalculator));
             calc = (ICalculator)mockCalc.Create();
-            Mocker mockReq = new Mocker(typeof(IHttpRequest));
-            req = (IHttpRequest) mockReq.Create();
+            Mocker mockReq = new Mocker(typeof(HttpRequest));
+            req = (HttpRequest) mockReq.Create();
         }
 
         [TestMethod]
-        public void TestCalculatorTypeName()
-        {
+        public void TestCalculatorTypeName() {
             string klassName = calc.ToString(); // OK ToString() inherited from Object
             Assert.AreEqual("MockICalculator", klassName);
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestCalculatorAddNotImplemented()
-        {
+        public void TestCalculatorAddNotImplemented() {
             calc.Add(2, 3);
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestCalculatorSubNotImplemented()
-        {
+        public void TestCalculatorSubNotImplemented() {
             calc.Sub(2, 3);
         }
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestCalculatorMulNotImplemented()
-        {
+        public void TestCalculatorMulNotImplemented() {
             calc.Mul(2, 3);
         }
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestCalculatorDivNotImplemented()
-        {
+        public void TestCalculatorDivNotImplemented() {
             calc.Div(2, 3);
         }
 
         [TestMethod]
-        public void TestRequestTypeName()
-        {
+        public void TestRequestTypeName() {
             string klassName = req.ToString(); // OK ToString() inherited from Object
             Assert.AreEqual("MockIHttpRequest", klassName);
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestRequestGetBodyNotImplemented()
-        {
+        public void TestRequestGetBodyNotImplemented() {
             req.GetBody("");
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotImplementedException))]
-        public void TestRequestDisposeNotImplemented()
-        {
+        public void TestRequestDisposeNotImplemented() {
             req.Dispose();
         }
     }
